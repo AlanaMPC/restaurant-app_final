f733d7bdb4e03e3ad8f75907ba56d4638b8d8a9d
anupama <anupamanairmail@gmail.com>
2026-01-05 20:46:44 +0530

    neww

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Zhaa Home Foods, Coimbatore</h1>
-      <p>
-        Inventive Indian cuisine compl
-      </p>
+      <div className="hero-content">
+        <h1>Zhaa Home Foods, Coimbatore</h1>
+        <p>
+          Inventive Indian cuisine complemented with global ingredients and
+          techniques.
+        </p>
+      </div>
     </section>
   );
 }
